16753aafff6f9e0abdbe02cd8503de64b4dc5bd9
Daydreamer <=>
2017-05-11 20:15:31 -0400

    add pods

---
+++ Pods/Target Support Files/Pods-GifSearcherTests/Pods-GifSearcherTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GifSearcherTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GifSearcherTestsVersionString[];
+
